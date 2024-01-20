be3382d7885baac760e6315af2e163b97b1fc80b
Jasim2334 <102066600+Jasim2334@users.noreply.github.com>
2024-01-20 18:34:12 +0530

    Delete deploy directory

--- deploy/demo1/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from demo1.models import Student
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['name','roll','subject']
--- deploy/demo1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from demo1.views import DeployView
-
-urlpatterns = [
-    path('student/',DeployView.as_view(),name='DeployApi')
-]
